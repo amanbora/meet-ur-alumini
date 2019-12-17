1af0710fadc2a064dd8e821c217b14bfe4e60d18
aman <bora.amanalambora@gmail.com>
2019-12-17 23:01:28 +0530

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
 
 const routes: Routes = [
+
   { path : 'home', component: HomeComponent },
-  { path : 'profile', component: ProfileComponent }
+  { path : 'profile', component: ProfileComponent },
+  { path : 'register', component: RegistrationComponent},
 ];
 
 @NgModule({
